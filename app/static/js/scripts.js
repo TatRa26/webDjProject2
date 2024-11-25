568c66641bd9533c0a40053bb11c02ec04610991
TatRa26 <tatra910@mail.ru>
2024-11-25 22:28:55 +0300

    Added templates (index.html, recipes.html), updated project settings, and fixed template rendering issues

---
+++ app/static/js/scripts.js
@@ -0,0 +1,19 @@
+// scripts.js
+
+// Плавная прокрутка к якорным ссылкам
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Отображение текущего года в футере
+document.addEventListener('DOMContentLoaded', () => {
+    const yearElement = document.getElementById('currentYear');
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+});
